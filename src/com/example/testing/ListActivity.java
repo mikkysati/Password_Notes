9b87ad0111f35b5558fad73788785da2a5068993
Mohit Sati <mikkysati@gmail.com>
2014-08-02 13:23:32 +0530

    Revert "just a comment"
    This reverts commit 0e2bd47265ade9d0bef196344a63309f47c8a762.

--- src/com/example/testing/ListActivity.java
+++ src/com/example/testing/ListActivity.java
@@ -37,7 +37,6 @@ public class ListActivity extends FragmentActivity {
 	public void onStop() {
 		super.onStop();
 		// closing the activity if screen is locked 
-		// importing projects to android studio
 		finish();
 		
 	}
